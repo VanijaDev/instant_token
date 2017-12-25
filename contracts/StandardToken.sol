eca6e078aafa80c63c0cb44e5bda3ddca04b5f76
Ivan Solomichev <vanijatko.social@gmail.com>
2017-12-25 11:58:15 +0200

    Added approve and allowance methods

--- contracts/StandardToken.sol
+++ contracts/StandardToken.sol
@@ -8,10 +8,23 @@ contract StandardToken is BasicToken, ERC20 {
 
   function transferFrom(address _from, address _to, uint _value) public onlyPayloadWithSize(3 * 32) {
     var _allowance = allowed[_from][msg.sender];
-    require (_value <= _allowance);
+//  no need in check _to & _value, because sub() handles it already.
     balances[_to] = balances[_to].add(_value);
     balances[_from] = balances[_from].sub(_value);
     allowed[_from][msg.sender] = _allowance.sub(_value);
     Transfer(_from, _to, _value);
+  }
+
+  function approve(address _spender, uint _value) public {
+    //  To update value, user needs firstly to set to 0 for sprender.
+    if ((_value != 0) && (allowed[msg.sender][_spender] != 0)) {
+      revert();
+    }
+    allowed[msg.sender][_spender] = _value;
+    Approval(msg.sender, _spender, _value);
+  }
+
+  function allowance(address _owner, address _spender) public constant returns (uint remaining) {
+    return allowed[_owner][_spender];
   }
 }
